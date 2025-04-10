15205016dd150f512180e7c791b2354219109d05
Ronny Perez <ronnypdev@gmail.com>
2025-04-09 21:47:19 -0400

    Added starter Files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import WeatherWidget from "./components/WeatherWidget"
+
+function App() {
+
+  return (
+    <>
+      <main className="h-dvh flex justify-center items-center">
+        <WeatherWidget />
+      </main>
+    </>
+  )
+}
+
+export default App
